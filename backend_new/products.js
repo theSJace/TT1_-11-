6db769c05fc3ecaed772130923e155989944e05d
Appleboyy <weeren97@gmail.com>
2021-09-30 12:00:33 +0800

    updated the products schema

--- backend_new/products.js
+++ backend_new/products.js
@@ -3,7 +3,14 @@ import mongoose from 'mongoose'
 const productSchema = mongoose.Schema({
     id: String,
     name: String,
-    description: String
+    description: String,
+    category: String,
+    image: String,
+    price: String,
+    countInStock: String,
+    brand: String,
+    rating: String,
+    numReviews: String
 })
 
 export default mongoose.model('products', productSchema)
